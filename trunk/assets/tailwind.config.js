21d34db60ee8d0b53c1b931cbed2cc0fcd0e09fe
brooklin.myers <brooklin.myers@b1343c55-5d13-cc01-28ab-ce57b7db7bff>
2021-10-31 21:57:15 +0000

    configure tailwind primary color default
    git-svn-id: https://github.com/J0hn1975/elixir_newbie_website.git@24 b1343c55-5d13-cc01-28ab-ce57b7db7bff

--- trunk/assets/tailwind.config.js
+++ trunk/assets/tailwind.config.js
@@ -1,10 +1,24 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: [
     './js/**/*.js',
     '../lib/*_web/**/*.*ex'
   ],
-  theme: {},
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: '#a800a1',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    }
+  },
   variants: {},
   plugins: []
 };
