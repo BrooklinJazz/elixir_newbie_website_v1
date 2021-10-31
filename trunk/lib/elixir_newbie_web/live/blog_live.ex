22579142e7079430bcfee93848360154dcd1df68
brooklin.myers <brooklin.myers@b1343c55-5d13-cc01-28ab-ce57b7db7bff>
2021-10-31 23:40:42 +0000

    style blog page
    git-svn-id: https://github.com/J0hn1975/elixir_newbie_website.git@25 b1343c55-5d13-cc01-28ab-ce57b7db7bff

--- trunk/lib/elixir_newbie_web/live/blog_live.ex
+++ trunk/lib/elixir_newbie_web/live/blog_live.ex
@@ -11,18 +11,40 @@ defmodule ElixirNewbieWeb.BlogLive do
   def render(assigns) do
     ~F"""
     <Page>
-    {#for blog <- @blogs}
     <section class="blogs">
-    <section class="blog-list">
-    <div class="blog">
-    <h2>{blog.title}</h2>
-    {blog.body_html}
-    </div>
+      <section class="blog-index">
+        <h2 class="text-3xl text-white pb-4 pl-4 border-b-2 border-solid border-primary">All Blog Posts</h2>
+        {#for blog <- @blogs}
+          <article
+            :on-click={"set_active_blog"}
+            phx-value-title={blog.title}
+            class={
+            "w-full h-20 flex justify-center items-center border-b-2 border-solid border-primary "
+            <> "transition duration-300 ease-in-out hover:bg-primary "
+            <> "#{blog.title === @active_blog.title && "bg-primary"}"
+            }>
+            <h3 class="text-xl text-white">{blog.title}</h3>
+          </article>
+        {/for}
+      </section>
+      <section class="blog-list">
+        <div class="blog">
+          <h2>{@active_blog.title}</h2>
+          {@active_blog.body_html}
+        </div>
+      </section>
     </section>
-    </section>
-    {/for}
     </Page>
     """
+  end
+
+  def handle_event("set_active_blog", %{"title" => title}, socket) do
+    %{blogs: blogs} = socket.assigns
+
+    {:noreply,
+     assign(socket,
+       active_blog: Enum.find(blogs, fn each -> each.title === title end)
+     )}
   end
 
   def blogs_to_html(blogs) do
@@ -37,8 +59,8 @@ defmodule ElixirNewbieWeb.BlogLive do
 
   def mount(_params, _session, socket) do
     PubSub.subscribe(ElixirNewbie.PubSub, @topic)
-
-    {:ok, assign(socket, blogs: blogs_to_html(BlogCache.get()))}
+    blogs = blogs_to_html(BlogCache.get())
+    {:ok, assign(socket, blogs: blogs, active_blog: List.first(blogs))}
   end
 
   def handle_info({:update_blogs, blogs}, socket),
