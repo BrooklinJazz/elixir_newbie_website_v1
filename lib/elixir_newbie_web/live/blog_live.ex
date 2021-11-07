6d8f0ed519f9ed188965c14c78617a5ae500aaa8
Brooklin Myers <brooklinjazzmyers@gmail.com>
2021-11-06 22:30:18 -0700

    Mobile styles and functionality

--- lib/elixir_newbie_web/live/blog_live.ex
+++ lib/elixir_newbie_web/live/blog_live.ex
@@ -8,29 +8,40 @@ defmodule ElixirNewbieWeb.BlogLive do
   @topic "update blogs"
 
   def render(assigns) do
+    IO.inspect(assigns.selected_blog_on_mobile, label: "BLOG")
+
     ~F"""
     <Page>
       <Feed>
         <:items>
-        <h2 class="pb-4 pl-4 text-3xl text-white border-b-2 border-solid border-primary">All Blog Posts</h2>
+        <h2 class={
+          "pb-4 pl-4 text-3xl text-white border-b-2 border-solid sm:pt-8 md:pt-0 border-primary "
+          <> "hidden md:flex"
+        } >Blog Posts</h2>
         {#for blog <- @blogs}
           <article
             :on-click={"set-active-blog"}
             phx-value-title={blog.title}
             class={
-            "w-full h-20 flex pl-4 items-center border-b-2 border-solid border-primary cursor-pointer text-white"
-            <> " border-secondary duration-300 ease-in-out hover:border-b-8 hover:text-secondary "
-            <> "#{blog.title === @active_blog.title && "border-secondary border-b-8 text-secondary"} "
+            "w-full h-20 flex pl-4 items-center border-b-2 border-solid border-primary cursor-pointer text-white "
+            <> "border-secondary duration-300 ease-in-out hover:border-b-8 hover:text-secondary "
+            <> "#{blog.title === @active_blog.title && "md:border-secondary md:border-b-8 md:text-secondary"} "
+            <> "#{@selected_blog_on_mobile && "hidden md:flex"} "
             }>
             <h3 class="text-xl text-white">{blog.title}</h3>
           </article>
         {/for}
         </:items>
         <:active_item>
-          <article class="p-4 markdown bg-surface">
-            <h2 class="pb-4">{@active_blog.title}</h2>
+          <article class={
+          "p-4 text-white bg-surface "
+          <> "#{!@selected_blog_on_mobile && "hidden md:block"} "
+          }>
+          <h2 class="pb-4 text-3xl text-white">{@active_blog.title}</h2>
+          <figure class="markdown">
             <img src={@active_blog.cover_image}/>
             {Blogs.as_highlighted_html(@active_blog)}
+          </figure>
           </article>
         </:active_item>
       </Feed>
@@ -47,7 +58,12 @@ defmodule ElixirNewbieWeb.BlogLive do
 
   def handle_params(%{"title" => title}, _url, socket) do
     %{blogs: blogs} = socket.assigns
-    {:noreply, assign(socket, active_blog: Enum.find(blogs, &(&1.title === title)))}
+
+    {:noreply,
+     assign(socket,
+       selected_blog_on_mobile: true,
+       active_blog: Enum.find(blogs, &(&1.title === title))
+     )}
   end
 
   def handle_params(_params, _url, socket) do
@@ -57,13 +73,21 @@ defmodule ElixirNewbieWeb.BlogLive do
   def mount(%{"title" => title}, _session, socket) do
     PubSub.subscribe(ElixirNewbie.PubSub, @topic)
     blogs = Blogs.get()
-    {:ok, assign(socket, blogs: blogs, active_blog: Enum.find(blogs, &(&1.title === title)))}
+
+    {:ok,
+     assign(socket,
+       blogs: blogs,
+       selected_blog_on_mobile: true,
+       active_blog: Enum.find(blogs, &(&1.title === title))
+     )}
   end
 
   def mount(_params, _session, socket) do
     PubSub.subscribe(ElixirNewbie.PubSub, @topic)
     blogs = Blogs.get()
-    {:ok, assign(socket, blogs: blogs, active_blog: List.first(blogs))}
+
+    {:ok,
+     assign(socket, blogs: blogs, selected_blog_on_mobile: false, active_blog: List.first(blogs))}
   end
 
   def handle_info({:update_blogs, blogs}, socket),
