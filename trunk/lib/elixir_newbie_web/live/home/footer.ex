578f59cdedb732c202b64bb0523dea2a8fe991e9
brooklin.myers <brooklin.myers@b1343c55-5d13-cc01-28ab-ce57b7db7bff>
2021-12-09 04:23:53 +0000

    restyle and connect footer component to real data
    git-svn-id: https://github.com/J0hn1975/elixir_newbie_website.git@57 b1343c55-5d13-cc01-28ab-ce57b7db7bff

--- trunk/lib/elixir_newbie_web/live/home/footer.ex
+++ trunk/lib/elixir_newbie_web/live/home/footer.ex
@@ -1,22 +1,28 @@
 defmodule ElixirNewbieWeb.Live.Home.Footer do
   use Surface.LiveView
+
+  alias ElixirNewbie.Blogs
+  alias ElixirNewbie.Podcast
+
+  alias ElixirNewbieWeb.BlogLive
+  alias ElixirNewbieWeb.Endpoint
   alias ElixirNewbieWeb.Live.Components.Paragraph
   alias ElixirNewbieWeb.Live.Components.Title
-  alias ElixirNewbieWeb.Router.Helpers, as: Routes
-  alias Surface.Components.LiveRedirect
-  alias ElixirNewbieWeb.Endpoint
+  alias ElixirNewbieWeb.Live.Components.IconButton
   alias ElixirNewbieWeb.PodcastLive
-  alias ElixirNewbieWeb.BlogLive
+  alias ElixirNewbieWeb.Router.Helpers, as: Routes
+
+  alias Surface.Components.LiveRedirect
 
   def render(assigns) do
     ~F"""
-      <section class="grid w-full min-h-full gap-24 mt-24 mb-24 sm:grid-cols-1 lg:grid-cols-4 sm:grid-rows-4 lg:grid-rows-1 sm:px-8 md:px-12">
-        <article class="flex flex-col items-center">
-          <Title>Elixir Newbie</Title>
+      <section class="grid w-full min-h-full gap-24 px-12 mt-24 mb-24 sm:grid-cols-1 xl:grid-cols-4 sm:grid-rows-4 xl:grid-rows-1 lg:grid-cols-2 lg:grid-rows-2 sm:px-8 md:px-12">
+        <article class="flex flex-col">
+          <Title class="text-center">Elixir Newbie</Title>
           <Paragraph>Elixir Newbie is dedicated to helping new developers grow. The Elixir Newbie website is a blog, a podcast, and a collection of my favourite resources.</Paragraph>
         </article>
-        <article class="flex flex-col items-center">
-          <Title>Resources</Title>
+        <article class="flex flex-col">
+          <Title class="text-center">Resources</Title>
           <Paragraph>If you’re new to Elixir, here’s a quick collection of some of my favourite tools and resources from the elixir community.</Paragraph>
           <Paragraph>Pretty much shout outs to some folks & projects I admire.</Paragraph>
           <ul class="w-full mt-6 text-xl leading-relaxed text-white underline list-disc list-inside">
@@ -27,11 +33,11 @@ defmodule ElixirNewbieWeb.Live.Home.Footer do
             <li><a target="_blank" href="https://elixircasts.io/">Elixir Casts</a></li>
           </ul>
         </article>
-        <article class="flex flex-col items-center">
-          <Title>Podcast</Title>
+        <article class="flex flex-col">
+          <Title class="text-center">Podcast</Title>
           <ul class="w-full mt-6 text-xl leading-relaxed text-white underline list-disc list-inside">
             {#for episode <- @highlighted_episodes}
-              <li>
+              <li class="mt-6">
                 <LiveRedirect
                   to={Routes.live_path(Endpoint, PodcastLive)}>
                   {episode.title}
@@ -39,12 +45,13 @@ defmodule ElixirNewbieWeb.Live.Home.Footer do
               </li>
             {/for}
           </ul>
+          <IconButton rounded={true} icon={:right_arrow} class={"mt-6"}>More Episodes</IconButton>
         </article>
-        <article class="flex flex-col items-center">
-          <Title>Blog</Title>
+        <article class="flex flex-col">
+          <Title class="text-center">Blog</Title>
           <ul class="w-full mt-6 text-xl leading-relaxed text-white underline list-disc list-inside">
             {#for article <- @highlighted_blogs}
-              <li>
+              <li class="mt-6">
                 <LiveRedirect
                   to={Routes.live_path(Endpoint, BlogLive)}>
                   {article.title}
@@ -52,18 +59,17 @@ defmodule ElixirNewbieWeb.Live.Home.Footer do
               </li>
             {/for}
           </ul>
+          <IconButton rounded={true} icon={:right_arrow} class={"mt-6"}>More Articles</IconButton>
         </article>
       </section>
     """
   end
 
   def mount(_params, _session, socket) do
-    # TODO, create real highlighted episodes
     {:ok,
      assign(socket,
-       highlighted_episodes:
-         Enum.map(1..12, fn index -> %{title: "Example Episode #{index} Title"} end),
-       highlighted_blogs: Enum.map(1..12, fn index -> %{title: "Example Blog #{index} Title"} end)
+       highlighted_episodes: Enum.take(Podcast.get(), 5),
+       highlighted_blogs: Enum.take(Blogs.get(), 5)
      )}
   end
 end
