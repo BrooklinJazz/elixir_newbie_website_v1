752993528e863c2f1fe834a274893626c7e57773
brooklin.myers <brooklin.myers@b1343c55-5d13-cc01-28ab-ce57b7db7bff>
2021-12-10 09:50:30 +0000

    add search and tag filtering to blog list
    git-svn-id: https://github.com/J0hn1975/elixir_newbie_website.git@61 b1343c55-5d13-cc01-28ab-ce57b7db7bff

--- trunk/test/test_helper.exs
+++ trunk/test/test_helper.exs
@@ -1,5 +1,3 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(ElixirNewbie.Repo, :manual)
 ExUnit.configure(exclude: [:prod, :skip])
-Mox.defmock(ElixirNewbie.MockBlogAPI, for: ElixirNewbie.BlogAPI)
-Application.put_env(:elixir_newbie, :blog_api, ElixirNewbie.MockBlogAPI)
