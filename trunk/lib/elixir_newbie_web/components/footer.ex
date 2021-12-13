50149fb418fe64a7de8023123222d4c665a33c6a
brooklin.myers <brooklin.myers@b1343c55-5d13-cc01-28ab-ce57b7db7bff>
2021-12-13 04:07:04 +0000

    fix footer highlighting
    git-svn-id: https://github.com/J0hn1975/elixir_newbie_website.git@87 b1343c55-5d13-cc01-28ab-ce57b7db7bff

--- trunk/lib/elixir_newbie_web/components/footer.ex
+++ trunk/lib/elixir_newbie_web/components/footer.ex
@@ -4,8 +4,10 @@ defmodule ElixirNewbieWeb.Components.Footer do
   """
   use Surface.LiveComponent
   alias ElixirNewbieWeb.BlogList
+  alias ElixirNewbieWeb.BlogShow
   alias ElixirNewbieWeb.Endpoint
   alias ElixirNewbieWeb.HomeLive
+  alias ElixirNewbieWeb.PodcastShow
   alias ElixirNewbieWeb.PodcastList
   alias ElixirNewbieWeb.Router.Helpers, as: Routes
 
@@ -13,22 +15,30 @@ defmodule ElixirNewbieWeb.Components.Footer do
     ~F"""
     <nav class="sticky bottom-0 flex flex-col md:hidden">
         <hr class="h-0.5 w-full border-0 navigation__hr bg-gradient-to-r from-primary to-secondary animate-gradient-x"/>
-        <nav class="flex items-center justify-around text-white bg-background">
-        <button :on-click="home" class="p-6">
+        <nav class="flex items-center justify-around bg-background">
+        <button :on-click="home" class={"p-6", "text-secondary": selected(assigns, HomeLive), "text-white": !selected(assigns, HomeLive)}>
           <i class="fas fa-home"/>
           <p>Home</p>
         </button>
-        <button :on-click="blog" class="p-6">
+        <button :on-click="blog" class={"p-6", "text-secondary": selected(assigns, [BlogList, BlogShow]), "text-white": !selected(assigns, [BlogList, BlogShow])}>
           <i class="fas fa-blog"></i>
           <p>Blog</p>
         </button>
-        <button :on-click="podcast" class="p-6">
+        <button :on-click="podcast" class={"p-6", "text-secondary": selected(assigns, [PodcastList, PodcastShow]), "text-white": !selected(assigns, [PodcastList, PodcastShow])}>
           <i class="fas fa-podcast"></i>
           <p>Podcast</p>
         </button>
       </nav>
     </nav>
     """
+  end
+
+  def selected(assigns, views) when is_list(views) do
+    assigns.socket.view in views
+  end
+
+  def selected(assigns, view) do
+    assigns.socket.view === view
   end
 
   def mount(_params, _session, socket) do
