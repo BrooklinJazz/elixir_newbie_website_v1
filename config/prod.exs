ef303c0d40caaa9cf2504824b14beadf7faaad6c
Brooklin Myers <brooklinjazzmyers@gmail.com>
2022-01-04 13:22:28 -0800

    undo force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,6 @@ import Config
 config :elixir_newbie, ElixirNewbieWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
