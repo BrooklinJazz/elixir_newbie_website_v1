446225bcdfc698a9e91afe9eb338254e134ac352
brooklin.myers <brooklin.myers@b1343c55-5d13-cc01-28ab-ce57b7db7bff>
2022-01-04 21:59:16 +0000

    undo force ssl and add new check origin
    git-svn-id: https://github.com/J0hn1975/elixir_newbie_website.git@115 b1343c55-5d13-cc01-28ab-ce57b7db7bff

--- trunk/config/runtime.exs
+++ trunk/config/runtime.exs
@@ -42,13 +42,11 @@ if config_env() == :prod do
   config :elixir_newbie, ElixirNewbieWeb.Endpoint,
     server: true,
     http: [port: {:system, "PORT"}],
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: 443],
     secret_key_base: secret_key_base,
-    force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
     check_origin: [
       "//www.elixirnewbie.com",
       "https://www.elixirnewbie.com",
-      "http://www.elixirnewbie.com",
       "https://elixir-newbie-staging.gigalixirapp.com"
     ]
 
