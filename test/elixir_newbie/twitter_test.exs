12339a4d0f217a49a204306467b1c4a5a6eda521
Axel Clark <axel.clark@pm.me>
2022-07-14 12:38:49 -0700

    Update test & add get_author/1
    * Update get/0 test for basic tweet info
    * Add test and function for get_author/1

--- test/elixir_newbie/twitter_test.exs
+++ test/elixir_newbie/twitter_test.exs
@@ -2,14 +2,13 @@ defmodule ElixirNewbie.TwitterTest do
   use ExUnit.Case, async: true
   alias ElixirNewbie.Twitter
 
-  test "ping api" do
-    result = Twitter.get()
-    assert result
-    assert is_list(result)
-    assert [tweet | _tail] = result
-    assert %{"id" => id, "text" => text} = tweet
-    assert is_bitstring(text)
-    assert is_bitstring(id)
-    assert text =~ ~r/#ElixirNewbie/i
+  test "get/0" do
+    assert [%{"id" => _, "text" => _, "created_at" => _, "author" => _} | _] = Twitter.get()
+  end
+
+  test "get_author/1" do
+    author_id = "1429815729533198339"
+
+    assert %{"id" => _, "username" => _, "name" => _} = Twitter.get_author(author_id)
   end
 end
