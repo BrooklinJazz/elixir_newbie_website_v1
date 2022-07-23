241294eb4080cb328be7c0a9eadc3f3b1fbec6d7
Brooklin <brooklinjazzmyers@gmail.com>
2022-07-23 12:49:22 -0700

    Add author info and additiona tweet info to ElixirNewbie.Twitter.get/0 Co-authored-by: FloatingDorito <FloatingDorito@users.noreply.github.com> Co-authored-by: Lucas Tan <lucasxtan@users.noreply.github.com> Co-authored-by: Russell Baker <threetriangles@users.noreply.github.com> Co-authored-by: Matthew Villwock <mtvillwock@gmail.com> Co-authored-by: Tom <BigTom@users.noreply.github.com> Co-authored-by: Vinod Kumar K <vinodkumark369@users.noreply.github.com>

--- lib/elixir_newbie/twitter.ex
+++ lib/elixir_newbie/twitter.ex
@@ -4,23 +4,33 @@ defmodule ElixirNewbie.Twitter do
     token = Application.fetch_env!(:elixir_newbie, :twitter_api_key)
 
     {:ok, response} = HTTPoison.get(@hashtag_elixir_newbie_url, Authorization: "Bearer #{token}")
+
     %HTTPoison.Response{body: body} = response
     {:ok, map} = JSON.decode(body)
-    %{"data" => data} = map
-    data
+
+    %{"data" => tweets} = map
+
+    Enum.map(tweets, fn %{"id" => id, "author_id" => author_id} = tweet ->
+      author = get_author(author_id)
+
+      Map.put(tweet, "author", author)
+      |> Map.put("tweet_url", "https://twitter.com/#{author["username"]}/status/#{id}")
+    end)
   end
 
   def get_author(author_id) do
     token = Application.fetch_env!(:elixir_newbie, :twitter_api_key)
 
     {:ok, response} =
-      HTTPoison.get("https://api.twitter.com/2/users/#{author_id}",
+      HTTPoison.get(
+        "https://api.twitter.com/2/users/#{author_id}?user.fields=profile_image_url,username",
         Authorization: "Bearer #{token}"
       )
 
     %HTTPoison.Response{body: body} = response
     {:ok, map} = JSON.decode(body)
-    %{"data" => data} = map
-    data
+    %{"data" => author} = map
+    %{"username" => username} = author
+    Map.put(author, "author_url", "https://twitter.com/#{username}")
   end
 end
